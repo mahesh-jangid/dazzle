c4ec1427e4b6799cdbb43a2005096b083a118238
narsimaxwell <narsijangid01@gmail.com>
2023-12-28 13:45:58 +0530

    changes

--- pages/Inbox.tsx
+++ pages/Inbox.tsx
@@ -58,8 +58,8 @@ const Inbox: NextPage = () => {
         ) : (
           <div />
         )}
-        <div className={`${ move ? "w-[0px]": "w-[100px]"
-        } h-[calc(100%-60px)] overflow-y-auto overflow-x-hidden dark:[color-scheme:dark] md:w-[350px]`}>
+        <div className={`"${ move ? "w-[0px]": "w-[100px]"
+        } h-[calc(100%-60px)] overflow-y-auto overflow-x-hidden dark:[color-scheme:dark] md:w-[350px]"`}>
           <div
             className={chatRoomLoading ? "fixed opacity-0" : ''}
           >
