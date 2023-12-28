5efcb9030a72ce47c18c50d8b4c9ec39203fc107
narsimaxwell <narsijangid01@gmail.com>
2023-12-28 14:20:50 +0530

    changessss

--- components/InboxPage/ChatRoom.tsx
+++ components/InboxPage/ChatRoom.tsx
@@ -16,11 +16,11 @@ interface Props {
   chatRoomID: string;
   userID: string;
   activeChat: string;
-  move: boolean;
+  move:Boolean;
   activeChatId: string;
 }
 
-function ChatRoom({ chatRoomID, move, userID, activeChat, activeChatId }: Props) {
+function ChatRoom({ chatRoomID,move, userID, activeChat, activeChatId }: Props) {
   const [darkMode] = useAtom(atoms.darkMode);
   const [allChatRoomMessages] = useAtom(atoms.allChatRoomMessages);
 
@@ -40,16 +40,15 @@ function ChatRoom({ chatRoomID, move, userID, activeChat, activeChatId }: Props)
   return (
     <div className="dark:text-slate-">
       <div
-        className={`${
-          activeChat === activeChatId ? 'flex' : 'hidden'
-        } absolute ${move ? `left-[0px]` : `left-[100px]`} top-0 h-[50px] cursor-default items-center gap-2 border-1 border-stone-300 pl-2 dark:border-stone-700 md:left-[350px] md:gap-4 md:pl-10`}
+        className={`${ activeChat === activeChatId ? 'flex' : 'hidden'
+        } absolute ${ move ? `left-[0px]`: `left-[100px]`} top-0 h-[50px] cursor-default items-center gap-2 border-1 border-stone-300 pl-2 dark:border-stone-700 md:left-[350px] md:gap-4 md:pl-10`}
       >
         {avatarURL === '' || !avatarURL ? (
-          <div className="h-7 w-7">
+          <div className='h-7 w-7'>
             <ProfilePicSVG strokeWidth="1.5" />
             <picture>
               <img
-                // unfortunately this image is needed to force map loading state to be triggered
+                // unfortuanetly this image is needed to force map loading state to be triggered
                 className="h-0 w-0 opacity-0"
                 src="https://imagizer.imageshack.com/img922/3706/Q1vJOp.png"
                 alt="avatar"
@@ -106,7 +105,7 @@ function ChatRoom({ chatRoomID, move, userID, activeChat, activeChatId }: Props)
         )}
       </div>
       {activeChat === activeChatId ? (
-        <div className={`absolute bottom-0' ${move ? `top-[5px]` : `top-[50px]`} ${move ? `left-[0px]` : `left-[100px]`} flex ${move ? `w-[calc(100%-3px)]` : `w-[calc(100%-130px)]`} cursor-default flex-col justify-end border-l border-t  border-stone-300 dark:border-stone-700 md:left-[350px] md:w-[calc(100%-350px)]`}>
+        <div className={`absolute bottom-0' ${ move ? `top-[5px]`: `top-[50px]`} ${ move ? `left-[0px]`: `left-[100px]`} flex ${move ? `w-[calc(100%-3px)]`:`w-[calc(100%-130px)]`} cursor-default flex-col justify-end border-l border-t  border-stone-300 dark:border-stone-700 md:left-[350px] md:w-[calc(100%-350px)]`}>
           <div className="flex cursor-default flex-col-reverse gap-5 overflow-y-auto px-1 py-2 dark:[color-scheme:dark] md:px-5">
             {messages.map((message, index) => (
               <div
